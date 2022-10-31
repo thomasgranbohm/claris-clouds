1327630ef0a8873a2d2d62636fbcd4811bb0e5e3
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-10-31 19:15:32 +0100

    Add slug field and custom findOne method

--- backend/src/api/artwork/controllers/artwork.ts
+++ backend/src/api/artwork/controllers/artwork.ts
@@ -2,6 +2,26 @@
  * artwork controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::artwork.artwork');
+export default factories.createCoreController(
+	"api::artwork.artwork",
+	({ strapi }) => ({
+		async findOne(ctx) {
+			const { id: slug } = ctx.params;
+
+			const entity = await strapi.db
+				.query("api::artwork.artwork")
+				.findOne({
+					where: {
+						slug,
+					},
+					populate: ["image"],
+				});
+
+			const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+			return this.transformResponse(sanitizedEntity);
+		},
+	})
+);
