b184d49934df844b7ec30c7d4316d701f4fc9e81
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-12-09 17:07:48 +0100

    Update ArtworkDisplay section

--- frontend/src/components/ComponentRenderer/components/ArtworkDisplay/index.tsx
+++ frontend/src/components/ComponentRenderer/components/ArtworkDisplay/index.tsx
@@ -16,40 +16,45 @@ const ArtworkDisplay: FC<ArtworkDisplaySchema> = ({ artworks, title }) => {
 	const stripped = stripWrapper(artworks);
 
 	return (
-		<Row className={classes["artwork-display"]} row-gap="none">
-			<Column>
-				<Heading type="h2">{title}</Heading>
-			</Column>
-			{stripped.map(({ image, name, slug }, i) => (
-				<Column sm={6} md={4} key={i} className={classes["artwork"]}>
-					<Link
-						href={`artwork/${slug}`}
-						className={classes["link"]}
-						asWrapper
+		<div className={classes["artwork-display"]}>
+			<Heading type="h2">{title}</Heading>
+			<Row>
+				{stripped.map(({ image, name, slug }, i) => (
+					<Column
+						sm={6}
+						md={4}
+						key={i}
+						className={classes["artwork"]}
 					>
-						<Image
-							className={classes["image"]}
-							alt={image.data.attributes.alternativeText}
-							height={image.data.attributes.height}
-							layout="fill"
-							objectFit="contain"
-							src={
-								image.data.attributes.hash +
-								image.data.attributes.ext
-							}
-							width={image.data.attributes.width}
-						/>
-						<Heading
-							type="b"
-							look="h6"
-							className={classes["title"]}
+						<Link
+							href={`artwork/${slug}`}
+							className={classes["link"]}
+							asWrapper
 						>
-							{name}
-						</Heading>
-					</Link>
-				</Column>
-			))}
-		</Row>
+							<Image
+								className={classes["image"]}
+								alt={image.data.attributes.alternativeText}
+								height={image.data.attributes.height}
+								layout="fill"
+								objectFit="contain"
+								src={
+									image.data.attributes.hash +
+									image.data.attributes.ext
+								}
+								width={image.data.attributes.width}
+							/>
+							<Heading
+								type="b"
+								look="h6"
+								className={classes["title"]}
+							>
+								{name}
+							</Heading>
+						</Link>
+					</Column>
+				))}
+			</Row>
+		</div>
 	);
 };
 
