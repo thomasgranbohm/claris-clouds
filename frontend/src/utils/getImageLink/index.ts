60d87c5bfd31f05ab8e5b8850756d408d2e02ad0
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-12-19 23:53:42 +0100

    Fix staging Image loading error

--- frontend/src/utils/getImageLink/index.ts
+++ frontend/src/utils/getImageLink/index.ts
@@ -1,10 +1,6 @@
-import getConfig from "next/config";
-
 import { ImageSchema } from "types/api/strapi";
 
-const { publicRuntimeConfig } = getConfig();
-
 const getImageLink = ({ ext, hash }: Pick<ImageSchema, "hash" | "ext">) =>
-	`${publicRuntimeConfig.PAGE_URL}/api/image/${hash}${ext}`;
+	`/api/image/${hash}${ext}`;
 
 export default getImageLink;
