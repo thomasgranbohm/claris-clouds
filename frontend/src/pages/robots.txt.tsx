870694a94592dc9a565dff9fedfce37e4af0d346
Thomas Granbohm <thomas.granbohm@gmail.com>
2023-01-14 19:24:32 +0100

    Add robots.txt page

---
+++ frontend/src/pages/robots.txt.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from "next";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+	const robots = `User-agent: *
+Allow: /
+
+Sitemap: https://${publicRuntimeConfig.PAGE_URL}/sitemap.xml`;
+
+	res.setHeader("Content-Type", "text/plain");
+	res.write(robots);
+	res.end();
+
+	return {
+		props: {},
+	};
+};
+
+const Robots = () => void 0;
+
+export default Robots;
