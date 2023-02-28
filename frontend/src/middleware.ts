7a0564f93373f29b0ebe989d5120427d5b2ae729
Thomas Granbohm <thomas.granbohm@gmail.com>
2023-02-28 01:02:23 +0100

    Update middleware and sitemap

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -14,6 +14,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!api|_next/static|_next/image|service-worker.js|ms-icon-70x70.png|ms-icon-310x310.png|ms-icon-150x150.png|ms-icon-144x144.png|manifest.json|favicon.ico|favicon-96x96.png|favicon-32x32.png|favicon-16x16.png|browserconfig.xml|apple-icon-precomposed.png|apple-icon.png|apple-icon-76x76.png|apple-icon-72x72.png|apple-icon-60x60.png|apple-icon-57x57.png|apple-icon-180x180.png|apple-icon-152x152.png|apple-icon-144x144.png|apple-icon-120x120.png|apple-icon-114x114.png|android-icon-96x96.png|android-icon-72x72.png|android-icon-48x48.png|android-icon-36x36.png|android-icon-192x192.png|android-icon-144x144.png|login).*)",
+		"/((?!api|login|sitemap\\.xml|robots\\.txt|_next/static|_next/image|service-worker.js|ms-icon-70x70.png|ms-icon-310x310.png|ms-icon-150x150.png|ms-icon-144x144.png|manifest.json|favicon.ico|favicon-96x96.png|favicon-32x32.png|favicon-16x16.png|browserconfig.xml|apple-icon-precomposed.png|apple-icon.png|apple-icon-76x76.png|apple-icon-72x72.png|apple-icon-60x60.png|apple-icon-57x57.png|apple-icon-180x180.png|apple-icon-152x152.png|apple-icon-144x144.png|apple-icon-120x120.png|apple-icon-114x114.png|android-icon-96x96.png|android-icon-72x72.png|android-icon-48x48.png|android-icon-36x36.png|android-icon-192x192.png|android-icon-144x144.png).*)",
 	],
 };
--- frontend/src/pages/sitemap.xml.tsx
+++ frontend/src/pages/sitemap.xml.tsx
@@ -1,52 +1,69 @@
 import { GetServerSideProps } from "next";
 import getConfig from "next/config";
 
-import { getArtworkSlugs } from "api/artwork";
-import { getPageSlugs } from "api/page";
+// import { getArtworkSlugs } from "api/artwork";
+// import { getPageSlugs } from "api/page";
 
-import stripWrapper from "utils/stripWrapper";
+// import stripWrapper from "utils/stripWrapper";
 
 const { publicRuntimeConfig } = getConfig();
 
+// export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+// 	const [artworks, pages] = await Promise.all([
+// 		(async () => {
+// 			const { data, error } = await getArtworkSlugs();
+
+// 			if (error) {
+// 				throw error;
+// 			}
+
+// 			return stripWrapper(data.artworks);
+// 		})(),
+// 		(async () => {
+// 			const { data, error } = await getPageSlugs();
+
+// 			if (error) {
+// 				throw error;
+// 			}
+
+// 			return stripWrapper(data.pages);
+// 		})(),
+// 	]);
+
+// 	const genXMLBlock =
+// 		<T extends { slug: string }>(prefix?: string) =>
+// 		({ slug }: T) =>
+// 			`<url><loc>${`${publicRuntimeConfig.PAGE_URL}/${
+// 				prefix ? `${prefix}/` : ""
+// 			}${slug}`}</loc></url>`;
+
+// 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+// <url>
+// 	<loc>${publicRuntimeConfig.PAGE_URL}</loc>
+// </url>
+// <url>
+// 	<loc>${publicRuntimeConfig.PAGE_URL}/shop</loc>
+// </url>
+// 	${artworks.map(genXMLBlock("artwork")).join("\n")}
+// 	${pages.map(genXMLBlock()).join("\n")}
+// </urlset>`;
+
+// 	res.setHeader("Content-Type", "text/xml");
+// 	res.write(sitemap);
+// 	res.end();
+
+// 	return {
+// 		props: {},
+// 	};
+// };
+
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-	const [artworks, pages] = await Promise.all([
-		(async () => {
-			const { data, error } = await getArtworkSlugs();
-
-			if (error) {
-				throw error;
-			}
-
-			return stripWrapper(data.artworks);
-		})(),
-		(async () => {
-			const { data, error } = await getPageSlugs();
-
-			if (error) {
-				throw error;
-			}
-
-			return stripWrapper(data.pages);
-		})(),
-	]);
-
-	const genXMLBlock =
-		<T extends { slug: string }>(prefix?: string) =>
-		({ slug }: T) =>
-			`<url><loc>${`${publicRuntimeConfig.PAGE_URL}/${
-				prefix ? `${prefix}/` : ""
-			}${slug}`}</loc></url>`;
-
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-	<loc>${publicRuntimeConfig.PAGE_URL}</loc>
-</url>
-<url>
-	<loc>${publicRuntimeConfig.PAGE_URL}/gallery</loc>
-</url>
-	${artworks.map(genXMLBlock("artwork")).join("\n")}
-	${pages.map(genXMLBlock()).join("\n")}
+	<url>
+		<loc>${publicRuntimeConfig.PAGE_URL}</loc>
+	</url>
 </urlset>`;
 
 	res.setHeader("Content-Type", "text/xml");
