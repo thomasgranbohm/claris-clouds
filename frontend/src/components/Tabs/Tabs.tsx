0ba75d64a63210153d344c29f3287fad52836155
Thomas Granbohm <thomas.granbohm@gmail.com>
2023-06-05 16:01:25 +0200

    Add product type selection in Shop

---
+++ frontend/src/components/Tabs/Tabs.tsx
@@ -0,0 +1,67 @@
+import { FC, useRef } from "react";
+import { useTab, useTabList, useTabPanel } from "react-aria";
+import { TabListProps, TabListState, useTabListState } from "react-stately";
+import { Node } from "@react-types/shared";
+import clsx from "clsx";
+
+import { Column, Row } from "components/Grid";
+
+import { WithClassname } from "types/components";
+
+import classes from "./Tabs.module.scss";
+
+interface TabsProps extends WithClassname, TabListProps<object> {}
+
+const Tabs: FC<TabsProps> = ({ className, ...props }) => {
+	const state = useTabListState(props);
+	const ref = useRef(null);
+	const { tabListProps } = useTabList(props, state, ref);
+
+	return (
+		<div className={clsx(classes["container"], className)}>
+			<div {...tabListProps} ref={ref}>
+				<Row>
+					<Column md={6} lg={[4, 2]} className={classes["header"]}>
+						<b>Product type:</b>
+						{Array.from(state.collection).map((item) => (
+							<Tab key={item.key} item={item} state={state} />
+						))}
+					</Column>
+				</Row>
+			</div>
+			<TabPanel key={state.selectedItem?.key} state={state} />
+		</div>
+	);
+};
+
+const Tab: FC<{ item: Node<object>; state: TabListState<object> }> = ({
+	item,
+	state,
+}) => {
+	const { key, rendered } = item;
+	const ref = useRef(null);
+	const { isSelected, tabProps } = useTab({ key }, state, ref);
+
+	return (
+		<div
+			{...tabProps}
+			ref={ref}
+			className={clsx(classes["tab"], isSelected && classes["selected"])}
+		>
+			{rendered}
+		</div>
+	);
+};
+
+const TabPanel: FC<{ state: TabListState<object> }> = ({ state, ...props }) => {
+	const ref = useRef(null);
+	const { tabPanelProps } = useTabPanel(props, state, ref);
+
+	return (
+		<div {...tabPanelProps} ref={ref}>
+			{state.selectedItem?.props.children}
+		</div>
+	);
+};
+
+export default Tabs;
--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -1,3 +1,4 @@
+import { Item } from "react-stately";
 import { Product } from "@shopify/hydrogen-react/storefront-api-types";
 import { NextPage } from "next";
 
@@ -10,6 +11,7 @@ import { ShopifyImage } from "components/Image";
 import Layout from "components/Layout";
 import { StyledLink } from "components/Link";
 import MetaData from "components/MetaData";
+import Tabs from "components/Tabs";
 
 import { Shopify } from "types/api/shopify";
 
@@ -38,41 +40,80 @@ const ShopPage: NextPage<ShopPageProps> = ({ products }) => {
 				title="Shop"
 				description="This is the shop page where you can by my artworks."
 			/>
-			{products.edges.map(({ node: product }, i) => (
-				<Row key={product.id}>
-					<Column md={6} lg={[4, 2]} align="center">
-						{product.featuredImage && (
-							// eslint-disable-next-line jsx-a11y/alt-text
-							<ShopifyImage
-								image={product.featuredImage}
-								aspectRatio={[1, 1]}
-								sizes="(min-width: 960px) 640px, (min-width: 768px) 750px, (min-width: 480px) 640px"
-								style={{
-									aspectRatio: "1 / 1",
-									height: "auto",
-									maxWidth: "100%",
-									objectFit: "cover",
-									verticalAlign: "bottom",
-									width: "100%",
-								}}
-							/>
-						)}
-					</Column>
-					<Column md={6} lg={4} align="center">
-						<Heading type="h2">{product.title}</Heading>
-						{product.descriptionHtml && (
-							<HtmlRenderer content={product.descriptionHtml} />
-						)}
-						<Row>
-							<Column lg={8}>
-								<StyledLink href={`/artwork/${product.handle}`}>
-									Buy a print
-								</StyledLink>
-							</Column>
-						</Row>
-					</Column>
-				</Row>
-			))}
+			<Tabs>
+				{Object.entries<Array<Product>>(
+					products.edges.reduce<Record<string, Array<Product>>>(
+						(p, { node: product }) => {
+							const { productType } = product;
+							const type = productType.endsWith("y")
+								? productType.substring(
+										0,
+										productType.length - 2
+								  ) + "ies"
+								: !productType.endsWith("s")
+								? productType + "s"
+								: productType;
+
+							if (type in p) {
+								p[type].push(product);
+							} else {
+								p[type] = [product];
+							}
+
+							return p;
+						},
+						{}
+					)
+				).map(([key, products]) => (
+					<Item key={key} title={key}>
+						{products.map((product) => (
+							<Row key={product.id}>
+								<Column md={6} lg={[4, 2]} align="center">
+									{product.featuredImage && (
+										// eslint-disable-next-line jsx-a11y/alt-text
+										<ShopifyImage
+											image={product.featuredImage}
+											aspectRatio={[1, 1]}
+											sizes="(min-width: 960px) 640px, (min-width: 768px) 750px, (min-width: 480px) 640px"
+											style={{
+												aspectRatio: "1 / 1",
+												height: "auto",
+												maxWidth: "100%",
+												objectFit: "cover",
+												verticalAlign: "bottom",
+												width: "100%",
+											}}
+										/>
+									)}
+								</Column>
+								<Column md={6} lg={4} align="center">
+									<Heading type="h2">{product.title}</Heading>
+									{product.descriptionHtml && (
+										<HtmlRenderer
+											content={product.descriptionHtml}
+										/>
+									)}
+									<Row>
+										<Column lg={8}>
+											<StyledLink
+												href={`/artwork/${product.handle}`}
+											>
+												Buy{" "}
+												{/^[aoiuye]/i.test(
+													product.productType
+												)
+													? "an"
+													: "a"}{" "}
+												{product.productType}
+											</StyledLink>
+										</Column>
+									</Row>
+								</Column>
+							</Row>
+						))}
+					</Item>
+				))}
+			</Tabs>
 		</Layout>
 	);
 };
